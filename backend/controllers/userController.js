9763e2a64d7ae9d9b6296f82a35818d6716330e5
annaaju <annajuliaprimo@gmail.com>
2024-03-03 14:37:07 -0300

    função de login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,4 @@
+const createUserToken = require('../helpers/create-user-token')
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 
@@ -54,9 +55,42 @@ module.exports = class userController {
 
         try{
             await newUser.save()
-            res.status(201).json({ message: 'Usuário cadastrado com sucesso!' })
+            await createUserToken(newUser, req, res)
         }catch(error){
             res.status(500).json({ message: error })
         }
     }
+
+    static async login(req, res){
+    
+        const {email, password} = req.body
+
+        if(!email){
+            res.status(422).json({message: 'Email obrigatório'})
+            return
+        }
+
+        if(!password){
+            res.status(422).json({message: 'Senha obrigatória'})
+            return
+        }
+
+         //check if user exists; email as a kind of id
+         const userExists = await User.findOne({email: email})
+         if(!userExists){
+             res.status(422).json({message: 'Não há usuário cadastrado com esse e-mail!'})
+            return
+         }
+
+         //check if password match with db password
+         const checkPassword = await bcrypt.compare(password, User.password)
+         if(!checkPassword){
+            res.status(422).json({message: 'Senha inválida!'})
+           return
+        }
+
+        await createUserToken(newUser, req, res)
+
+    }
+
 }
