93d0e82ae115d601abc725202592c6a8cb49c656
annaaju <annajuliaprimo@gmail.com>
2024-03-04 12:21:05 -0300

    finalizando a parte do usuário

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,12 @@
-const createUserToken = require('../helpers/create-user-token')
-const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
+
+const createUserToken = require('../helpers/create-user-token')
+const getToken = require('../helpers/get-token')
+const getUserByToken = require('../helpers/get-user-by-token')
+
+const user = require('../models/user')
+const User = require('../models/user')
 
 module.exports = class userController {
     
@@ -89,7 +95,117 @@ module.exports = class userController {
            return
         }
 
-        await createUserToken(newUser, req, res)
+        await createUserToken(user, req, res)
+
+    }
+
+    static async checkUser(req, res){
+        
+        let currentUser
+
+        if(req.headers.authorization){
+
+            const token = getToken(req)
+            const decoded = jwt.verify(token, 'nossosecret')
+
+            currentUser = await User.findById(decoded.id)
+
+            currentUser.password = undefined
+
+        }else{
+            currentUser = null
+        }
+
+        res.status(200).send(currentUser)
+    }
+
+    static async getUserById(req, res){
+
+        const id = req.params.id
+
+        const user = await User.findById(id).select('-password')
+
+        if(!user){
+            res.status(422).json({message: 'Usuário não encontrado!'})
+           return
+        }
+
+        res.status(200).json({user})
+
+    }
+
+    static async editUser(req, res){
+        
+        const id = req.params.id
+
+        //checking if user exists
+        const token = getToken(req)
+        const user =  await getUserByToken(token)
+
+        const{name, email, phone, password, confirmpassword} = req.body
+
+        if(req.file){
+            user.image = req.file.filename
+        }
+
+        //validations
+        if(!name){
+            res.status(422).json({message: 'Nome obrigatório'})
+            return
+        }
+
+        user.name = name
+
+        if(!email){
+            res.status(422).json({message: 'Email obrigatório'})
+            return
+        }
+
+        //checking if email has already taken
+        const userExists = await User.findOne({email: email})
+
+        if(user.email !== email && userExists){
+            res.status(422).json({message: 'Usuário não encontrado'})
+            return 
+        }
+
+        user.email = email
+
+        if(!phone){
+            res.status(422).json({message: 'Telefone obrigatório'})
+            return
+        }
+
+        user.phone = phone
+
+        if(password !== confirmpassword){
+            res.status(422).json({message: 'As senhas não conferem!'})
+            return
+        }else if(password === confirmpassword && password != null){
+            //creating new password
+            const salt = await bcrypt.genSalt(15)
+            const passwordHash = await bcrypt.hash(password, salt)
+
+            user.password = passwordHash
+        }
+
+        try{
+
+            //returns users uptade data
+            await User.findOneAndUpdate(
+                {_id: user._id},
+                {$set: user},
+                {new: true},
+            )
+
+            res.status(200).json({message: 'Dados atualizados com sucesso!'})
+
+        }catch (err){
+
+            res.status(500).json({message: err})
+            return
+
+        }
 
     }
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,14 @@ const router = require('express').Router()
 
 const userController = require('../controllers/userController')
 
+//middlewares
+const verifyToken = require('../helpers/verify-token')
+const { imageUpload } = require('../helpers/image-upload')
+
 router.post('/register', userController.register)
 router.post('/login', userController.login)
+router.get('/checkuser', userController.checkUser)
+router.get('/:id', userController.getUserById)
+router.patch('/edit/:id', verifyToken, imageUpload.single("image"),  userController.editUser)
 
 module.exports = router
